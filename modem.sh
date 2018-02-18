0cf3b915acc3a3ce2afb4a5921710710f93e7f60
Jannis Pinter <jannis@pinterjann.is>
2018-02-18 17:36:16 +0100

    Updated to 18.02.0
    Updated to 18.02.0, included security imrprovements and implemented support for OSX (thanks to @jfdhuiz)

--- modem.sh
+++ modem.sh
@@ -2,35 +2,43 @@
 set -e
 
 ### constants
-VERSION="18.01.1"
-URL_MANUAL="https://storage.googleapis.com/fairphone-updates/3bdd65b8-33c9-4a10-ab07-7765c2419fe5/FP2-gms-$VERSION-manual.zip"
-URL_OTA="https://storage.googleapis.com/fairphone-updates/3bdd65b8-33c9-4a10-ab07-7765c2419fe5/FP2-gms-18.01.1-ota-from-17.12.1.zip"
+VERSION="18.02.0"
 
-CHECKSUM_MANUAL="c505adf745f178e2cbef8f7cfd4bf18c37bdd7f1ae525792d0bae762b5d77988"
-CHECKSUM_OTA="a4a4f4f856e08f37024c00c7b697d1bbd6b213249c48192141bb8db60bccef51"
+URL_MANUAL="https://storage.googleapis.com/fairphone-updates/7ecf1675-541e-4720-84fa-110a59464021/FP2-gms-18.02.0-manual.zip"
+URL_OTA="https://storage.googleapis.com/fairphone-updates/7ecf1675-541e-4720-84fa-110a59464021/FP2-gms-18.02.0-ota-from-18.01.1.zip"
 
+CHECKSUM_MANUAL="c364cd8b56da41f020cc4eb6e76594ccc9fa2c3cf83a7b54917f37630178c87c"
+CHECKSUM_OTA="ab56f2af2ae87ef8e95a43b3b46c0bef6a469098f59e386f4cd1aee08ed0b25e"
+
+### check platform
+unamestr="$(uname)"
+if [[ "$unamestr" == "Darwin" ]]; then
+   alias sha256sum='gsha256sum'
+fi
 
 ### print welcome message
 echo "Fairphone modem.zip generator"
 
 ### download an official FP2 ota zip that contains the 'update-binary'
-mkdir -p /tmp/modem/firmware-update
-curl --progress-bar $URL_OTA -o /tmp/ota.zip
+umask 0077
+MODEMDIR="$(mktemp -d /tmp/modem.XXXXXXXX)"
+
+curl --progress-bar "$URL_OTA" -o /tmp/ota.zip
 echo "$CHECKSUM_OTA /tmp/ota.zip" | sha256sum -c
-unzip /tmp/ota.zip META-INF/* -d /tmp/modem/
+unzip /tmp/ota.zip META-INF/* -d "$MODEMDIR"
 rm /tmp/ota.zip
 
 ### we don't need the updater-script provided by Fairphone
-rm /tmp/modem/META-INF/com/google/android/updater-script
+rm "$MODEMDIR"/META-INF/com/google/android/updater-script
 
 ### download manual.zip containing the latest proprietary binaries
-curl --progress-bar $URL_MANUAL -o /tmp/manual.zip
+curl --progress-bar "$URL_MANUAL" -o /tmp/manual.zip
 echo "$CHECKSUM_MANUAL /tmp/manual.zip" | sha256sum -c
-unzip -j /tmp/manual.zip images/rpm.mbn images/emmc_appsboot.mbn images/NON-HLOS.bin images/tz.mbn images/splash.img images/sbl1.mbn -d /tmp/modem/firmware-update/
+unzip -j /tmp/manual.zip images/rpm.mbn images/emmc_appsboot.mbn images/NON-HLOS.bin images/tz.mbn images/splash.img images/sbl1.mbn -d "$MODEMDIR"/firmware-update/
 rm /tmp/manual.zip
 
 ### write updater-script
-cat > /tmp/modem/META-INF/com/google/android/updater-script <<EOF
+cat > "$MODEMDIR"/META-INF/com/google/android/updater-script <<EOF
 
 get_device_compatible("FP2") == "OK" || abort("This package is for \"FP2\" devices; this is a \"" + getprop("ro.product.device") + "\".");
 set_progress(0.200000);
@@ -59,16 +67,17 @@ package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/platform/msm_sd
 EOF
 
 ### inject version number
-echo "ui_print(\"Flashing successful! You have updated your modem firmware to $VERSION.\");" >> /tmp/modem/META-INF/com/google/android/updater-script
-echo "set_progress(1.000000);" >> /tmp/modem/META-INF/com/google/android/updater-script
+echo "ui_print(\"Flashing successful! You have updated your modem firmware to "$VERSION".\");" >> "$MODEMDIR"/META-INF/com/google/android/updater-script
+echo "set_progress(1.000000);" >> "$MODEMDIR"/META-INF/com/google/android/updater-script
 
 ### zip it
-CURRENT_DIR=$(pwd)
-pushd /tmp/modem/
-zip -r "$CURRENT_DIR/modem-$VERSION.zip" META-INF/* firmware-update/*
+CURRENT_DIR="$(pwd)"
+pushd "$MODEMDIR"
+find "$MODEMDIR" -exec touch -t 201401010000 {} +
+zip -X -r "$CURRENT_DIR/modem-$VERSION.zip" META-INF/* firmware-update/*
 popd
-rm -r /tmp/modem/
+rm -r "$MODEMDIR"
 
 ### finish
-sha256sum modem-$VERSION.zip
+sha256sum modem-"$VERSION".zip
 echo "Done!"
